af7b0b90c08e2a9b922444e96ee6387ffe56d7bf
KarthikeyaniS <karthikeyani@corestack.io>
2023-03-07 21:12:11 +0530

    something

---
+++ angular/src/app/bootstrap/daterange-picker/daterange-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daterange-picker',
+  templateUrl: './daterange-picker.component.html',
+  styleUrls: ['./daterange-picker.component.css']
+})
+export class DaterangePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular/src/app/dummy/tree-node/tree-node.component.ts
+++ angular/src/app/dummy/tree-node/tree-node.component.ts
@@ -7,6 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TreeNodeComponent implements OnInit {
 
+  // treeDate={
+  //   name:'kjhg',
+  //   checked:false,
+  //   children:{
+  //     name:'khjg',
+  //     checked:false,
+  //     grandchild:{
+  //       name:'kjhg',
+  //       checked:false,
+
+  //     },
+     
+
+  //   }
+  // }
+
   treeData = [
     {
       name: 'Root Node 1',
@@ -19,6 +35,22 @@ export class TreeNodeComponent implements OnInit {
             {
               name: 'gc1',
               checked: false,
+              level3: [
+                {
+                  name: 'jhfg',
+                  checked: false,
+                  level4: [
+                    {
+                      name: 'devilkk',
+                      checked: false,
+                    },
+                    {
+                      name: 'd2hgf',
+                      checked: false,
+                    }
+                  ]
+                }
+              ]
             },
             {
               name: 'gc2',
@@ -29,14 +61,24 @@ export class TreeNodeComponent implements OnInit {
         {
           name: 'Child Node 2',
           checked: false,
-          grandChild:[
+          grandChild: [
             {
               name: 'gc4',
               checked: false,
-              level3:[
+              level3: [
                 {
-                  name:'kk',
-                  checked:false,
+                  name: 'kk',
+                  checked: false,
+                  level4: [
+                    {
+                      name: 'devil',
+                      checked: false,
+                    },
+                    {
+                      name: 'd2',
+                      checked: false,
+                    }
+                  ]
                 }
               ]
             },
@@ -51,6 +93,55 @@ export class TreeNodeComponent implements OnInit {
     },
 
   ];
+
+  checkboxTrigger(i: any, arry: any,a:any) {
+
+    // console.log(this.treeData)
+    console.log(i.currentTarget.checked);
+    // console.log();
+
+    console.log(arry[0].children[0].grandChild);
+    // if(arr[i])
+
+    console.log('lvl3',arry[0].children[0].grandChild[0].level3);
+    
+
+    console.log(arry[0].currentTarget);
+    if(arry[0]==this.treeData){
+
+    }   
+    this.findPath(i,arry,a);
+    this.master(i, arry);
+  }
+
+  findPath(i:any,arry:any,k:any){
+      console.log(k)
+      console.log('kjhvcghjkl;kjhgfygvbnhjnm');
+      
+  }
+
+
+  master(i: any, arry: any) {
+
+
+    for (var j = 0; j < arry[0].children.length; j++) {
+      arry[0].children[j].checked = i.currentTarget.checked;
+      for (var k = 0; k < arry[0].children[0].grandChild.length; k++) {
+        arry[0].children[j].grandChild[k].checked = i.currentTarget.checked;
+        // console.log(arry[0].children[j].grandChild[k].level3[1]);
+        
+        // for (var l = 0; l < arry[0].children[0].grandChild[0].level3.length; l++) {
+        //   // console.log(arry[0].children[j].grandChild[j].level3[]);
+          
+        //   arry[0].children[j].grandChild[k].level3[j].checked = i.currentTarget.checked;
+        // }
+
+      }
+
+
+    }
+
+  }
 
   constructor() { }
 
